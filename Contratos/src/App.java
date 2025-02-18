7bd3a38b148afbddd3fa56b500f8bd92a4c213f1
Leonardo <leonardo.albuquerque.melo@gmail.com>
2025-02-18 10:25:10 -0300

    Classes desenvolvidas

--- Contratos/src/App.java
+++ Contratos/src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.HourContract;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter department's name: ");
+        String nameDepartament = sc.nextLine();
+
+        
+
+        sc.close();
     }
 }
---
+++ Contratos/src/Entities/Department.java
@@ -0,0 +1,28 @@
+package Entities;
+
+public class Department {
+  private String name;
+
+
+  public Department() {
+  }
+
+  public Department(String name){
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Department [Name()=" + getName() + "]";
+  }
+
+  
+}
