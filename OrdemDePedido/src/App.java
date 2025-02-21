19d2af4fb18d59fe22c3661cb004a549754f0025
Leonardo <leonardo.albuquerque.melo@gmail.com>
2025-02-21 09:46:50 -0300

    classes prontas

--- OrdemDePedido/src/App.java
+++ OrdemDePedido/src/App.java
@@ -1,8 +1,12 @@
+import java.sql.Date;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Client;
 import entities.Order;
+import entities.OrderItem;
+import entities.Product;
 import entities.enums.OrderStatus;
 
 
@@ -10,14 +14,47 @@ public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-         
-        Client client = new Client("Alex Green", "alex@gmail.com", Client.parseDate("15/03/1985"));
-        Order order = new Order(LocalDateTime.now(), OrderStatus.PENDING_PAYMENT, client);
+        
+
+        System.out.println("Enter client data: ");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        System.out.println("e-mail: ");
+        String email = sc.nextLine();
+        System.out.println("Birth date (DD/MM/YYYY)");
+        String birthDate = sc.nextLine();
+
+        Client client = new Client(name, email, Client.parseDate(birthDate));
+
+        System.out.println("Enter order data: ");
+        System.out.println("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.nextLine());
+        
+        LocalDateTime dateNow = LocalDateTime.now();    
+        Order order = new Order(dateNow, status, client);
+
+        System.out.println("How many items to this order?");
+        int qnt = sc.nextInt();
+
+        for(int i=0;i<qnt;i++){
+            System.out.println("Enter #" + (i+1) + " item data: ");
+            System.out.println("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.println("Product price: ");
+            double productPrice = sc.nextDouble();
+            System.out.println("Quantity: ");
+            int qntProduct = sc.nextInt();
+
+            Product product = new Product(productName, productPrice);
+            OrderItem orderItem = new OrderItem(qntProduct, productPrice, product);
+            
+            order.addItem(orderItem);
+        }
+
+        System.out.println(order);
 
 
-     
-
-        System.out.println(order.toString());
 
     }
 }
